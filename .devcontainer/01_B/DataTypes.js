b58a60117fe179b14489f84510d91d1d26d88c45
pandurangnagure07 <123914527+pandurangnagure07@users.noreply.github.com>
2026-06-17 11:01:06 +0000

    Data_Types

---
+++ .devcontainer/01_B/DataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // Treat all JS code as no=ewer version
+
+/*
+number 2 to power53
+bigInt
+string --> ""
+boolean --> true / false
+null --> Standalone value //Object
+undefined --> undefined
+symbol --> Unique
+*/
+
+
+
+
+
+//alert(3+3) //we are using "node.js" Not Browser
+
+let name="Pandurang" //DT- String
+let age =3  //DT- int
+let isLoggedIn = true //DT - Boolean
+
+//Object
+console.log(typeof undefined)  // "typeof" -- is ued for to check type of the variable or the value
