93079fc52f03b0366b393907b77e38f547767893
pandurangnagure07 <123914527+pandurangnagure07@users.noreply.github.com>
2026-06-19 13:40:49 +0000

    Object2_done

--- .devcontainer/01_B/09_2_Object.js
+++ .devcontainer/01_B/09_2_Object.js
@@ -1,3 +1,4 @@
+//lec-17 & lec-18
 //const tinderUser = new Object()
 //console.log(tinderUser)
 
@@ -23,6 +24,77 @@ const regularUser ={
 
 const obj1 ={1:'a',2:'b'}
 const obj2 ={3:'a',4:'b'}
+const obj4 ={5:'a',6:'b'}
 
-const obj3 ={obj1,obj2}
-console.log(obj3) 
+//const obj3 ={obj1,obj2}
+//const obj3 =Object.assign({},obj1,obj2,obj4)
+
+const obj3 ={...obj1,...obj2,...obj4}
+// console.log(obj3) 
+
+const users =[
+    {id:1,
+     email:"xyz@gmail.com"   
+    },
+    {
+      id:2,
+     email:"abc@gmail.com"    
+    },
+     {id:1,
+     email:"xyz@gmail.com"   
+    },
+    {
+      id:2,
+     email:"abc@gmail.com"    
+    }
+     ,
+     {id:3,
+     email:"xyz@gmail.com"   
+    },
+    {
+      id:4,
+     email:"abc@gmail.com"    
+    }
+]
+
+
+users[1].email
+// console.log(tinderUser)
+
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+
+
+// ---------------------------------------------lec 18--------------------------------------
+
+const course={
+        courseName:"Js in hindi",
+        price:'999',
+        courseInstructor:"Pandurang"
+}
+
+const{courseInstructor}=course //This is first way to check instructor or obj
+console.log(courseInstructor)
+
+const{courseInstructor:CIN}=course //This is second way to check instructor or obj
+console.log(CIN)
+
+
+/*
+this is related to destructing in React.js
+const navBar=({company})=>{
+        console.log(navBar)
+}
+
+navBar(company="Pandurang")
+
+--------This is JSON format----- 
+{
+    name:"pandurang",
+    courseName:"JS in hindi",
+    price:"free "
+}
+    */
