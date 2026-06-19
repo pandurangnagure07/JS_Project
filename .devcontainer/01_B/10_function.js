152a1921d718cf04ee9cc649db260691c90d2252
pandurangnagure07 <123914527+pandurangnagure07@users.noreply.github.com>
2026-06-19 14:17:18 +0000

    Fuction

---
+++ .devcontainer/01_B/10_function.js
@@ -0,0 +1,23 @@
+//Lecture--19   
+
+function sayMyName(){
+console.log("P")
+console.log("A")
+console.log("N")
+console.log("D")
+console.log("U")
+console.log("R")
+console.log("A")    
+console.log("N")   
+console.log("G")  
+}
+
+sayMyName  //This is reference
+sayMyName() //
+
+function addTwoNumbers(num1,num2){
+   console.log(num1+num2) 
+}
+addTwoNumbers(59,82)
+addTwoNumbers(59,"A")
+addTwoNumbers(59,null)
